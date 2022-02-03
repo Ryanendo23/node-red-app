7bbf51b7886893a4f8fe271d549cf41a845631bd
Ryanendo23 <98942174+Ryanendo23@users.noreply.github.com>
2022-02-03 02:18:25 -0600

    Update health.js

--- routers/health.js
+++ routers/health.js
@@ -10,4 +10,14 @@ module.exports = function (app) {
   });
 
   app.use('/health', router);
-}
+}var languages = ["JavaScript", "Python", "C++", "Java", "Lua"];
+console.log("Original Array: " + languages);
+
+// reversing array order
+reversed_arr = languages.reverse();
+
+// reverse() modifies the original array
+console.log("Array after reverse(): " + languages);
+
+// reverse() also returns the reversed array
+console.log("Return Value of reverse(): " + reversed_arr);
